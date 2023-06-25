ce44e6901a077496c18dfd7083aa46ff96679f90
Philipp Mayer <phlppmayer@gmail.com>
2023-06-25 21:20:29 +0200

    return exceptions happening in db

--- src/main/kotlin/de/p10r/ArtistsStore.kt
+++ src/main/kotlin/de/p10r/ArtistsStore.kt
@@ -1,15 +1,18 @@
 package de.p10r
 
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.resultFrom
 import dev.forkhandles.values.ofOrNull
 
 class ArtistsStore(database: Database) {
   private val sql = database.artistQueries
 
-  fun findAll(): List<Artist> =
-    sql.selectAll().executeAsList().toArtists()
+  fun findAll(): Result4k<List<Artist>, Exception> =
+    resultFrom { sql.selectAll().executeAsList().toArtists() }
 
-  fun create(newArtist: NewArtist): Artist =
-    sql.create(newArtist.name).executeAsOne().toArtist()
+  fun create(newArtist: NewArtist): Result<Artist, Exception> =
+    resultFrom { sql.create(newArtist.name).executeAsOne().toArtist() }
 
   private fun Stored_artists.toArtist() = Artist(ArtistId.ofOrNull(id)!!, name)
 
---
+++ src/test/kotlin/de/p10r/DatabaseTests.kt
@@ -0,0 +1,59 @@
+package de.p10r
+
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import dev.forkhandles.result4k.Success
+import dev.forkhandles.values.ofOrNull
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.lang.reflect.Proxy
+
+/**
+ * creates a proxy class which can be delegated to
+ * Takes care of overriding every method that has not been overridden with TODO
+ */
+inline fun <reified T> fake(className: String): T = Proxy.newProxyInstance(
+  T::class.java.classLoader,
+  arrayOf(T::class.java)
+) { _, _, _ ->
+  TODO("$className: Not implemented!")
+} as T
+
+fun Database.Companion.new() = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).let {
+  Schema.create(it)
+  Database(it)
+}
+
+fun ArtistId.Companion.unsafe(id: Long) = ofOrNull(id)!!
+
+
+class DatabaseTests {
+  val db = Database.new()
+  val store = ArtistsStore(db)
+  val sql = db.artistQueries
+
+  @Test
+  fun `find all stored artists`() {
+    sql.create("Boys Noize").executeAsOne()
+    sql.create("Daft Punk").executeAsOne()
+
+    assertEquals(
+      Success(
+        listOf(
+          Artist(ArtistId.unsafe(1), "Boys Noize"),
+          Artist(ArtistId.unsafe(2), "Daft Punk")
+        )
+      ),
+      store.findAll()
+    )
+  }
+
+  @Test
+  fun `insert a new artist`() {
+    store.create(NewArtist("Me"))
+
+    assertEquals(
+      Success(listOf(Artist(ArtistId.unsafe(1), "Me"))),
+      store.findAll()
+    )
+  }
+}
